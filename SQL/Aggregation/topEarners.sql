0ab25775290817ed6fa59de7808150d70681e757
shriram-s6 <86624773+shriram-s6@users.noreply.github.com>
2021-07-03 02:50:29 +0530

    Top Earners

---
+++ SQL/Aggregation/topEarners.sql
@@ -0,0 +1,7 @@
+/*
+We define an employee's total earnings to be their monthly salary * monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table.
+Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. 
+Then print these values as  space-separated integers.
+*/
+
+select max(salary * months), count(salary * months) from employee group by (salary * months) having (salary * months) = (select max(salary * months) from employee); 
