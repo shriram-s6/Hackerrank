8e901d702f2356c41f6de3914019d3c322559995
shriram-s6 <86624773+shriram-s6@users.noreply.github.com>
2021-07-02 20:39:12 +0530

    Weather Observation Station 10

---
+++ SQL/Basic Select/weatherObservation10.sql
@@ -0,0 +1,13 @@
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+select distinct(city) from station where (city not like '%a' and
+                                          city not like '%e' and
+                                          city not like '%i' and
+                                          city not like '%o' and
+                                          city not like '%u');
+
+/* Another solution:
+
+select distinct(city) from station where regexp_like(city, '[^aeiou]$');
+
+*/
