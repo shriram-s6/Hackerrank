1c6bb78beaec475b1a452ba344a827387dcf624d
shriram-s6 <86624773+shriram-s6@users.noreply.github.com>
2021-07-02 19:09:17 +0530

    Weather Observation Station 6

---
+++ SQL/Basic Select/weatherObservation6.sql
@@ -0,0 +1,14 @@
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+select distinct(city) from station where (city like 'A%' or 
+                                city like 'E%' or  
+                                city like 'I%' or  
+                                city like 'O%' or 
+                                city like 'U%');
+
+
+/* Another solution:
+
+select distinct(city) from station where regexp_like(city, '^[AEIOU]');
+
+*/
